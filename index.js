f2250e9536da57ffe016e6bfeaf3de49aeaf7df6
ehmicky <ehmicky@users.noreply.github.com>
2019-12-07 00:28:59 +0100

    Update event handler name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const pluginDecrypt = require('./pluginDecrypt')
 module.exports = {
   name: '@netlify/plugin-encrypted-files',
   // scopes: ['listSites'],
-  init() {
+  onInit() {
     console.log('decrypting files')
     pluginDecrypt()
   },
