0c1f9bec49adfded3a43535b3c57d1ea151f3525
swyx <shawnthe1@gmail.com>
2020-03-11 17:39:05 -0400

    add branches feature

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const pluginDecrypt = require('./pluginDecrypt');
+const chalk = require('chalk');
 
 module.exports = function netlify404nomore(conf) {
   return {
     name: 'netlify-plugin-encrypted-files',
-    onInit() {
+    onInit({ pluginConfig: { branches } }) {
       console.log('decrypting files');
-      pluginDecrypt({});
+      if (branches && branches.includes(process.env.BRANCH)) {
+        pluginDecrypt({});
+      } else if (typeof branches === 'undefined') {
+        pluginDecrypt({});
+      } else {
+        console.log(
+          'branches specified',
+          chalk.yellow(branches.join(' ')),
+          `but our current branch is ${chalk.blue(
+            process.env.BRANCH
+          )}, skipping decryption`
+        );
+      }
     }
   };
 };
